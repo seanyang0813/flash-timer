e62c1074c54582b538df2466ba306e1843edb219
seanyang0813 <seanyang0813@gmail.com>
2023-01-26 03:24:04 -0800

    first commit

---
+++ components/player.tsx
@@ -0,0 +1,65 @@
+import { EnvelopeIcon, PhoneIcon } from "@heroicons/react/20/solid";
+import Image from "next/image";
+import flashImg from "../assets/flash.png";
+import topImage from "../assets/top.png";
+import jungleImage from "../assets/jungle.png";
+import midImage from "../assets/mid.png";
+import botImage from "../assets/bot.png";
+import supportImage from "../assets/support.png";
+
+const roles = ["top", "jungle", "mid", "bot", "support"];
+
+const role_image_map = {
+  top: topImage,
+  jungle: jungleImage,
+  mid: midImage,
+  bot: botImage,
+  support: supportImage,
+};
+
+export const Player = ({ index, timers, updateFunction }) => {
+  const role = roles[index];
+
+  function subtract_10_sec() {
+    if (timers[index] == null) {
+      return;
+    }
+    updateFunction(Math.max(timers[index] - 100, 0));
+  }
+
+  function use_flash() {
+    updateFunction(3000);
+  }
+
+  return (
+    <div className="grid grid-cols-3 grid-rows-2">
+      <div className="col-span-1 row-span-2">
+        <Image
+          className="object-contain rounded-full"
+          src={role_image_map[role]}
+          alt={role}
+        ></Image>
+      </div>
+      <div className="col-span-2 row-span-1">
+        <h1 className="text-xl font-bold text-gray-300">{role}</h1>
+        <div className="justify-center flex">
+          <button>
+            <Image
+              src={flashImg}
+              width={50}
+              height={50}
+              alt={"flash icon"}
+              onClick={() => use_flash()}
+            ></Image>
+          </button>
+          <button className="text-gray-300" onClick={subtract_10_sec}>
+            -10
+          </button>
+          <p className="text-xl font-bold text-gray-300">
+            {timers[index] / 10}
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
