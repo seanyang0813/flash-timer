57a1fda79e851595e23a2958a1d7720cf440d1a9
seanyang0813 <32753704+seanyang0813@users.noreply.github.com>
2026-09-04 12:23:27 -0700

    Load clarity-first theorem styles

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,5 @@
 import "../styles/globals.css";
-import "../styles/matsumoto3d.css";
-import "../styles/matsumotoMobileFix.css";
+import "../styles/matsumotoClarity.css";
 import type { AppProps } from "next/app";
 
 export default function App({ Component, pageProps }: AppProps) {
